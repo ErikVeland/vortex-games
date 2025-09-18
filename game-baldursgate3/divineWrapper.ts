85ad116002dadb29c5de469c378878b3f7c7dda9
Erik Veland <erik.veland@news.com.au>
2025-09-18 15:59:47 +1000

    Cherry pick commit

--- game-baldursgate3/divineWrapper.ts
+++ game-baldursgate3/divineWrapper.ts
@@ -103,7 +103,9 @@ async function divine(api: types.IExtensionApi,
       }
       if (!stdoutStr && action !== 'list-package') {
         return resolve({ stdout: '', returnCode: 2 })
-      }      
+      }
+      
+      const stdoutStr = stdout.toString();
       if (['error', 'fatal'].some(x => stdoutStr.toLowerCase().startsWith(x))) {
         // Really?
         return reject(new Error(`divine.exe failed: ${stdoutStr}`));
